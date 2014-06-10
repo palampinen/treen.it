a45fb4796c28566f8c29a9fbd4707448c090c84b
thedpr <pasi.lampinen@deeper.fi>
2014-06-10 23:47:41 +0300

    some gauge testing in Tarkka-page

--- js/controllers.js
+++ js/controllers.js
@@ -128,6 +128,34 @@ if(window.localStorage['didTutorial'] === "true") {
 
 .controller('FriendsCtrl', function($scope, Friends) {
   $scope.friends = Friends.all();
+  
+  
+  // GAUGE TEST
+  	var opts = {
+	  lines: 12, // The number of lines to draw
+	  angle: 0.5, // The length of each line
+	  lineWidth: 0.06, // The line thickness
+	/*  pointer: {
+		length: 0.9, // The radius of the inner circle
+		strokeWidth: 0.035, // The rotation offset
+		color: '#000000' // Fill color
+	  },*/
+	  limitMax: 'true',   // If true, the pointer will not go past the end of the gauge
+	  colorStart: '#fff',   // Colors
+	  colorStop: '#fff',    // just experiment with them
+	  strokeColor: '#666',   // to see which ones work best for you
+	  generateGradient: false
+	};
+	var target = document.getElementById('gauge1'); // your canvas element
+	var gauge = new Donut(target).setOptions(opts); // create sexy gauge!
+	gauge.maxValue = 7; // set max gauge value
+	gauge.animationSpeed = 32; // set animation speed (32 is default value)
+	gauge.set(5);
+	
+  
+  
+  
+  
 })
 
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
